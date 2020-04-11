ba93e79b4a93e8e0492352d87c7349856f04271b
nrepak <nazarii.repak@binary-studio.com>
2020-04-11 18:44:00 +0300

    Basic styles and default components

--- App.js
+++ App.js
@@ -4,7 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello, World!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.boldText}>
+          Lorem ipsum dolor <Text>test</Text> sit amet.
+        </Text>
+        <Text>Lorem ipsum dolor sit amet. </Text>
+        <Text>Lorem ipsum dolor sit amet. </Text>
+      </View>
     </View>
   );
 }
@@ -15,5 +24,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20,
   },
 });
