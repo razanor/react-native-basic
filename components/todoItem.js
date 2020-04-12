aeda20dba6e1661df7121c74aff1b85018204128
nrepak <nazarii.repak@binary-studio.com>
2020-04-12 16:47:37 +0300

    Icons example

--- components/todoItem.js
+++ components/todoItem.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const TodoItem = ({ item, pressHandler }) => {
   return (
     <TouchableOpacity onPress={() => pressHandler(item.key)}>
-      <Text style={styles.item}>{item.text}</Text>
+      <View style={styles.item}>
+        <MaterialIcons name='delete' size={18} color='#333' />
+        <Text style={styles.itemText}>{item.text}</Text>
+      </View>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   item: {
+    flexDirection: 'row',
     padding: 16,
     marginTop: 16,
     borderColor: '#bbb',
     borderWidth: 1,
     borderStyle: 'dashed',
     borderRadius: 10,
+  },
+  itemText: {
+    marginLeft: 10,
   },
 });
 
